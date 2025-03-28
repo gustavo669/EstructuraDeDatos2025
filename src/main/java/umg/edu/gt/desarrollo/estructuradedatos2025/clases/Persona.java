dc3692c9612ab61fde151c1f24eefd5d9e14c31f
Gustavo Adolfo Godoy Barrera <ggodoyb1@miumg.edu.gt>
2025-03-27 18:55:56 -0600

    lombok
    Signed-off-by: Gustavo Adolfo Godoy Barrera <ggodoyb1@miumg.edu.gt>

---
+++ src/main/java/umg/edu/gt/desarrollo/estructuradedatos2025/clases/Persona.java
@@ -0,0 +1,14 @@
+package umg.edu.gt.desarrollo.estructuradedatos2025.clases;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persona {
+    private String nombre;
+    private int edad;
+}
+
